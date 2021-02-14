901c09e2e2ea8692325327fb9d05b1fa8c28d10c
sergey-noskov-73 <sergey.noskov.73@gmail.com>
2021-02-15 01:09:36 +0300

    Updates

---
+++ precache-manifest.318a5724e305aeed43f4b516859ce167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "664c3ae835dbf80747e0",
+    "url": "/hackathon/static/css/main.56d13078.chunk.css"
+  },
+  {
+    "revision": "664c3ae835dbf80747e0",
+    "url": "/hackathon/static/js/main.9cbbbbe8.chunk.js"
+  },
+  {
+    "revision": "10852034049cb3cd2679",
+    "url": "/hackathon/static/js/runtime~main.170e41e9.js"
+  },
+  {
+    "revision": "3a4330cb833b76035108",
+    "url": "/hackathon/static/css/2.6d40ea99.chunk.css"
+  },
+  {
+    "revision": "3a4330cb833b76035108",
+    "url": "/hackathon/static/js/2.58d5610f.chunk.js"
+  },
+  {
+    "revision": "229e44eac7d3d65fe1c364e223a6aeb1",
+    "url": "/hackathon/index.html"
+  }
+];
